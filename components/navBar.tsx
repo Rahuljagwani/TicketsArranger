1df5050ab712befbb8eccf6e0a1a155de94e37bd
Rahuljagwani <rahuljagwani1012@gmail.com>
2023-12-23 23:47:31 +0530

    minor fix

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -41,67 +41,71 @@ const NavigationBar: React.FC = () => {
   };
 
   return (
-    <AppBar position="static" color="default" className={'h-20 pt-1 bg-' + theme + '-navbg text-' + theme + '-text'}>
-      <Toolbar>
-        <Button
-          variant="outlined"
-          color="inherit"
-          onClick={handleMenuClick}
-          className='h-6 p-3 navButton'
-        >
-          <FormatListBulletedIcon className='size-4'/>
-          <span>
-          &nbsp;&nbsp;Display&nbsp;&nbsp;
-          </span>
-          <KeyboardArrowDownIcon
-            className='size-4'
-            style={{ transform: `rotate(${rotation}deg)`, transition: 'transform 0.3s ease-in-out' }}
-          />
-        </Button>
-        <Menu
-          id="display-menu"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleMenuClose}
-        >
-          <MenuItem>
-            <Typography variant="inherit">Group&nbsp;&nbsp;</Typography>
-            <Select
-              labelId="demo-select-small-label"
-              id="demo-select-small"
-              value={groupBy}
-              label="Group"
-              onChange={handleGrouping}
-              size='small'
-              className='w-28'
-            >
-              <MenuItem value="priority">Priority</MenuItem>
-              <MenuItem value="userId">Users</MenuItem>
-              <MenuItem value="status">Status</MenuItem>
-            </Select>
-          </MenuItem>
-          <MenuItem>
-            <Typography variant="inherit">Sort&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
-            <Select
-              labelId="demo-select-small-label"
-              id="demo-select-small"
-              value={orderBy}
-              label="Order"
-              onChange={handleOrdering}
-              size='small'
-              className='w-28'
-            >
-              <MenuItem value="title">Title</MenuItem>
-              <MenuItem value="priority">Priority</MenuItem>
-            </Select>
-          </MenuItem>
-        </Menu>
-        <div className='grow' />
-        <IconButton color="inherit" onClick={handleTheme}>
-          {isDarkMode ? <LightThemeIcon /> : <DarkThemeIcon />}
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <>
+      <div className='text-dark-text bg-dark-mainbg'></div>
+      <div className='bg-dark-navbg'></div>
+      <AppBar position="static" color="default" className={'h-20 pt-1 bg-' + theme + '-navbg text-' + theme + '-text'}>
+        <Toolbar>
+          <Button
+            variant="outlined"
+            color="inherit"
+            onClick={handleMenuClick}
+            className='h-6 p-3 navButton'
+          >
+            <FormatListBulletedIcon className='size-4' />
+            <span>
+              &nbsp;&nbsp;Display&nbsp;&nbsp;
+            </span>
+            <KeyboardArrowDownIcon
+              className='size-4'
+              style={{ transform: `rotate(${rotation}deg)`, transition: 'transform 0.3s ease-in-out' }}
+            />
+          </Button>
+          <Menu
+            id="display-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem>
+              <Typography variant="inherit">Group&nbsp;&nbsp;</Typography>
+              <Select
+                labelId="demo-select-small-label"
+                id="demo-select-small"
+                value={groupBy}
+                label="Group"
+                onChange={handleGrouping}
+                size='small'
+                className='w-28'
+              >
+                <MenuItem value="priority">Priority</MenuItem>
+                <MenuItem value="userId">Users</MenuItem>
+                <MenuItem value="status">Status</MenuItem>
+              </Select>
+            </MenuItem>
+            <MenuItem>
+              <Typography variant="inherit">Sort&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Typography>
+              <Select
+                labelId="demo-select-small-label"
+                id="demo-select-small"
+                value={orderBy}
+                label="Order"
+                onChange={handleOrdering}
+                size='small'
+                className='w-28'
+              >
+                <MenuItem value="title">Title</MenuItem>
+                <MenuItem value="priority">Priority</MenuItem>
+              </Select>
+            </MenuItem>
+          </Menu>
+          <div className='grow' />
+          <IconButton color="inherit" onClick={handleTheme}>
+            {isDarkMode ? <LightThemeIcon /> : <DarkThemeIcon />}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
